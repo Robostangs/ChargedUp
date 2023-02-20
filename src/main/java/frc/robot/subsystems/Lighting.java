6e4664ca5b95baac2089474f1162b8e70c5c1f0d
Ardusa <ardusa05@gmail.com>
2023-02-20 17:39:28 -0500

    Added blinken stuff

---
+++ src/main/java/frc/robot/subsystems/Lighting.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Lighting extends SubsystemBase {
+    private static Lighting mLighting;
+    
+    private final Spark blinken1;
+    private final Spark blinken2;
+
+    public static Lighting getInstance() {
+        if (mLighting == null) {
+            mLighting = new Lighting();
+        }
+        return mLighting;
+    }
+
+    public Lighting() {
+        blinken2 = new Spark(1);
+        blinken1 = new Spark(0);
+    }
+
+    public void setLights(double x) {
+        blinken1.set(x);
+        blinken2.set(x);
+    }
+}
