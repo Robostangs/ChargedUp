c7f2fe402d27658da43df23a7464d4b413eb122d
Robostangs-Software <122229138+Robostangs-Software@users.noreply.github.com>
2023-03-11 10:18:14 -0500

    Fix percent output

--- src/main/java/frc/robot/commands/Arm/PercentOutput.java
+++ src/main/java/frc/robot/commands/Arm/PercentOutput.java
@@ -2,28 +2,26 @@ package frc.robot.commands.Arm;
 
 import java.util.function.DoubleSupplier;
 
-import edu.wpi.first.util.datalog.DataLog;
-import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Arm;
 
 public class PercentOutput extends CommandBase {
-    double shoulder, elbow;
+    DoubleSupplier shoulder, elbow;
     private Arm mArm = Arm.getInstance();
 
     public PercentOutput(DoubleSupplier shoulder, DoubleSupplier elbow) {
-        this.shoulder = shoulder.getAsDouble();
-        this.elbow = elbow.getAsDouble();
+        this.shoulder = shoulder;
+        this.elbow = elbow;
     }
 
     @Override
     public void initialize() {
-        DataLogManager.log("Shoulder: " + shoulder);
-        DataLogManager.log("Elbow: " + elbow);
+        // System.out.println("Shoulder: " + shoulder);
+        // System.out.println("Elbow: " + elbow);
     }
 
     @Override
     public void execute() {
-        // mArm.setPower(shoulder, elbow);
+        mArm.setPower(shoulder.getAsDouble(), elbow.getAsDouble());
     }
 }
