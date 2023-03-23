be216212fad06d5215ed4a0c530276670523b88f
17acres <dan@17acr.com>
2023-03-22 22:48:48 -0400

    before throwing away my baddder path planning

--- src/main/java/frc/ArmTrajectoryPlanner/ArmTrajectoryPlanner.java
+++ src/main/java/frc/ArmTrajectoryPlanner/ArmTrajectoryPlanner.java
@@ -2,7 +2,23 @@ package frc.ArmTrajectoryPlanner;
 
 import java.util.ArrayList;
 
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.PathPoint;
+import com.pathplanner.lib.PathPlannerTrajectory.PathPlannerState;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.util.Color8Bit;
+import frc.robot.Constants;
 import frc.robot.Utils.Vector2D;
+import frc.robot.subsystems.Arm;
 
 public class ArmTrajectoryPlanner {
 
@@ -45,11 +61,21 @@ public class ArmTrajectoryPlanner {
         Vector2D deltaVector=endPoint.getSubtracted(startPoint);
         pathPoints.add(startPoint);
         pathVelocities.add(new Vector2D());
-        System.out.println("t,X,Y,Vx,Vy,Ax,Ay");
-        //plan forward
+        pathAccelerations.add(new Vector2D());
+        pathAngles.add(Arm.getInstance().calculateArmAngles(startPoint));
+        pathAngularVelocities.add(new Vector2D());
+        pathAngularAccelerations.add(new Vector2D());
+        System.out.println("t,X,Y,Vx,Vy,Ax,Ay,E,S,Ve,Vs,Ae,As");
+
+        PathPlannerTrajectory path = PathPlanner.generatePath(
+            new PathConstraints(targetMaxSpeed, targetMaxAccel), 
+            new PathPoint(new Translation2d(startPoint.x,startPoint.y),Rotation2d.fromDegrees(0)),
+            new PathPoint(new Translation2d(endPoint.x,endPoint.y),Rotation2d.fromDegrees(45)));
         for (double t=0;t<10;t+=sampleTime) {
             Vector2D lastPosition=pathPoints.get(pathPoints.size()-1);
             Vector2D lastVelocity=pathVelocities.get(pathVelocities.size()-1);
+            Vector2D lastAngles=pathAngles.get(pathAngles.size()-1);
+            Vector2D lastAngularVelocity=pathAngularVelocities.get(pathAngularVelocities.size()-1);
             double targetAcceleration=0;
             double currentSpeed = lastVelocity.getLength(); 
             if(lastPosition.distance(startPoint)<accelerationDistance){
@@ -63,13 +89,28 @@ public class ArmTrajectoryPlanner {
             Vector2D currentAcceleration = deltaVector.getNormalized().getMultiplied(targetAcceleration);
             Vector2D currentVelocity = lastVelocity.getAdded(currentAcceleration.getMultiplied(sampleTime));
             Vector2D currentPosition = lastPosition.getAdded(currentVelocity.getMultiplied(sampleTime));
+
+            Vector2D currentArmAngles = Arm.getInstance().calculateArmAngles(currentPosition);
+            Vector2D currentAngularVelocities = currentArmAngles.getSubtracted(lastAngles).getDivided(sampleTime);
+            Vector2D currentAnglularAccelerations = currentAngularVelocities.getSubtracted(lastAngularVelocity).getDivided(sampleTime);
+
+
             pathPoints.add(currentPosition);
             pathVelocities.add(currentVelocity);
             pathAccelerations.add(currentAcceleration);
-            System.out.println(t+","+currentPosition.x+","+currentPosition.y+","+currentVelocity.x+","+currentVelocity.y+","+currentAcceleration.x+","+currentAcceleration.y);
+            pathAngles.add(currentArmAngles);
+            pathAngularVelocities.add(currentAngularVelocities);
+            pathAngularAccelerations.add(currentAnglularAccelerations);
+            PathPlannerState state = (PathPlannerState) path.sample(t);
+            //System.out.printf("%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f\n", t,currentPosition.x,currentPosition.y, currentVelocity.x,currentVelocity.y,currentAcceleration.x, currentAcceleration.y, currentArmAngles.x, currentArmAngles.y, currentAngularVelocities.x, currentAngularVelocities.y, currentAnglularAccelerations.x, currentAnglularAccelerations.y);
+            System.out.printf("%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f\n", t,currentPosition.x,currentPosition.y, state.poseMeters.getX(), state.poseMeters.getY(),currentVelocity.x,currentVelocity.y,state.poseMeters.getRotation().getCos()*state.velocityMetersPerSecond,state.poseMeters.getRotation().getSin()*state.velocityMetersPerSecond,currentAcceleration.x, currentAcceleration.y,state.poseMeters.getRotation().getCos()*state.accelerationMetersPerSecondSq,state.poseMeters.getRotation().getSin()*state.accelerationMetersPerSecondSq);
+
+            //System.out.println(t+","+currentPosition.x+","+currentPosition.y+","+currentVelocity.x+","+currentVelocity.y+","+currentAcceleration.x+","+currentAcceleration.y);
         }
         pathPoints.add(endPoint);
         pathVelocities.add(new Vector2D());
+        pathAngles.add(Arm.getInstance().calculateArmAngles(endPoint));
+        pathAngularVelocities.add(new Vector2D());
     }
 
     public Vector2D getStartPoint() {
@@ -130,6 +171,29 @@ public class ArmTrajectoryPlanner {
 
     public ArrayList<Vector2D> getPathAngularAccelerations() {
         return pathAngularAccelerations;
+    }
+
+    public void simulateToLog() {
+         Mechanism2d mMechanismTarget = new Mechanism2d(Constants.Hand.maxFrameExtension.x*2, Constants.Hand.maxFrameExtension.y);
+     MechanismRoot2d mMechanismTargetRoot;
+     MechanismLigament2d mMechanismTargetShoulder;
+     MechanismLigament2d mMechanismTargetElbow;
+        mMechanismTargetRoot = mMechanismTarget.getRoot("ArmRoot", Constants.Hand.maxFrameExtension.x, 0);
+        mMechanismTargetShoulder = mMechanismTargetRoot.append(
+                new MechanismLigament2d("Shoulder", Constants.Arm.upperarmLength, 0, 2, new Color8Bit(Color.kRed)));
+        mMechanismTargetElbow = mMechanismTargetShoulder.append(
+                new MechanismLigament2d("Elbow", Constants.Arm.forearmLength, 0, 2, new Color8Bit(Color.kRed)));
+        for(int i=0;i<pathPoints.size();i++){
+            mMechanismTargetElbow.setAngle(pathAngles.get(i).getElbow());
+            mMechanismTargetShoulder.setAngle(pathAngles.get(i).getShoulder());
+            SmartDashboard.putData("sim arm",mMechanismTarget);
+            try {
+                Thread.sleep((long)(sampleTime*1000));
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
     
 
